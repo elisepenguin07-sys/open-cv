07b7abe0cb2fc3dbcdd1041d1af468c5d10e0e95
Elise <elise.peng.uin07@gmail.com>
2025-10-07 11:41:56 +0800

    Update youtube hot music 2025.py

--- youtube hot music 2025.py
+++ youtube hot music 2025.py
@@ -9,8 +9,10 @@ df = pd.read_csv("youtube-top-100-songs-2025.csv")
 st.subheader("Youtube Top 100 songs 2025")
 st.write(df['title'])
 
-plt.scatter(df['duration'], df['view_count']) 
-plt.xlabel('Duration (seconds)') 
-plt.ylabel('View Count') 
-plt.title('View Count vs Duration') 
-st.pyplot() 
+fig, ax = plt.subplots()  # 建立畫布和座標軸
+ax.scatter(df['duration'], df['view_count'])  # 在座標軸上畫散點圖
+ax.set_xlabel('Duration (seconds)')  # 設定 x 軸標籤
+ax.set_ylabel('View Count')  # 設定 y 軸標籤
+ax.set_title('View Count vs Duration')  # 設定圖表標題
+
+st.pyplot(fig)
