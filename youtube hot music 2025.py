d855d85ccc41c27d921a4ce9e0f8b31d8971a88e
Elise <elise.peng.uin07@gmail.com>
2025-10-07 11:20:40 +0800

    Update youtube hot music 2025.py

--- youtube hot music 2025.py
+++ youtube hot music 2025.py
@@ -1,5 +1,6 @@
 # data-analysis
 import pandas as pd
+import matplotlib.pyplot as plt
 import streamlit as st
 
 st.header("YouTube Hot Music 2025")
@@ -7,3 +8,8 @@ df = pd.read_csv("youtube-top-100-songs-2025.csv")
 
 st.subheader("Youtube Top 100 songs 2025")
 st.write(df['title'])
+
+plt.scatter(df['view_count'],df['duration'])
+plt.xlabel("View count")
+plt.ylabel("Duration(seconds)")
+st.pyplot() 
