eb3f17271570e19bbf491bdb685da8c51a10ba5a
Elise <elise.peng.uin07@gmail.com>
2025-10-07 11:38:48 +0800

    Update youtube hot music 2025.py

--- youtube hot music 2025.py
+++ youtube hot music 2025.py
@@ -9,10 +9,8 @@ df = pd.read_csv("youtube-top-100-songs-2025.csv")
 st.subheader("Youtube Top 100 songs 2025")
 st.write(df['title'])
 
-fig, ax = plt.subplots()  # 建立 figure 和 axis
-ax.scatter(df['duration'], df['view_count'])
-ax.set_xlabel('Duration (seconds)')
-ax.set_ylabel('View Count')
-ax.set_title('View Count vs Duration')
-
-st.pyplot(fig)
+plt.scatter(df['duration'], df['view_count']) 
+plt.xlabel('Duration (seconds)') 
+plt.ylabel('View Count') 
+plt.title('View Count vs Duration') 
+st.pyplot() 
