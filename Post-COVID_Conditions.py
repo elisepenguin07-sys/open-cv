d5f4d6099a9036f9199c94e2e04687d99677bd7f
Elise <elise.peng.uin07@gmail.com>
2025-10-07 13:59:19 +0800

    Update Post-COVID_Conditions.py

--- Post-COVID_Conditions.py
+++ Post-COVID_Conditions.py
@@ -8,3 +8,9 @@ df = pd.read_csv("Post-COVID_Conditions.csv")
 
 st.subheader("Missing Data")
 st.write("To understand which data are harder to collect, we will first identify the most challenging ones.")
+df_na = df.isna().sum()
+
+fig1, ax1 = plt.subplots()
+ax1.hist = (df_na, bins=8, linewidth=0.5, edgecolor="white")
+ax1.set_xlabel = ("Missing Data Columns")
+st.plot(fig1)
