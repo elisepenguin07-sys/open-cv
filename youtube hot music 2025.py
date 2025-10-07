64ec2411fa310dfcadc11f40074abed8f5388983
Elise <elise.peng.uin07@gmail.com>
2025-10-07 11:06:33 +0800

    Update youtube hot music 2025.py

--- youtube hot music 2025.py
+++ youtube hot music 2025.py
@@ -1,1 +1,9 @@
 # data-analysis
+import pandas as pd
+import steamlit
+
+st.header("YouTube Hot Music 2025")
+df = pd.read_csv("youtube-top-100-songs-2025.csv")
+
+st.subheader("Youtube Top 100 songs 2025")
+st.write(df.['title'])
